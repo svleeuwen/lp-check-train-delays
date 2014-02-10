108eda43a5c5b1200eec011e7fcc26ec61dbb07c
Sander van Leeuwen <replytosander@gmail.com>
2014-02-11 00:33:14 +0100

    celery tasks (forgot add) api poll queue work in progress images

--- nsapi/api.py
+++ nsapi/api.py
@@ -12,7 +12,7 @@ class NSApi(object):
     TIME_FROM = '08:55'
     TIME_UNTIL = '09:10'
 
-    BASE_URL = "http://webservices.ns.nl/ns-api-treinplanner?fromStation=%(from_station)s&toStation=%(to_station)s&dateTime=%(date_time)s&hslAllowed=%(include_highspeed)s&previousAdvices=%(num_previous_advices)s"
+    BASE_URL = "http://webservices.ns.nl/ns-api-treinplanner?fromStation=%(from_station)s&toStation=%(to_station)s&dateTime=%(departure_time)s&hslAllowed=%(include_highspeed)s&previousAdvices=%(num_previous_advices)s"
 
     def __init__(self, auth_string):
         self.auth_string = base64.urlsafe_b64encode(auth_string)
@@ -20,7 +20,7 @@ class NSApi(object):
     def get(self, from_station, to_station, departure_time, include_highspeed='true', num_previous_advices=0):
         url = self.BASE_URL % dict(from_station=from_station,
                                    to_station=to_station,
-                                   date_time=departure_time,
+                                   departure_time=self._strftime(departure_time),
                                    include_highspeed=include_highspeed,
                                    num_previous_advices=num_previous_advices)
 
@@ -39,15 +39,18 @@ class NSApi(object):
                 #pass
                 continue
             delays.append({
-                'departure_planned': self._parse_time(option.find('GeplandeVertrekTijd').text),
-                'departure_planned_str': self._parse_time(option.find('GeplandeVertrekTijd').text).strftime('%H:%M'),
-                'departure_actual': self._parse_time(option.find('ActueleVertrekTijd').text),
-                'departure_actual_str': self._parse_time(option.find('ActueleVertrekTijd').text).strftime('%H:%M'),
+                'departure_planned': self._strptime(option.find('GeplandeVertrekTijd').text),
+                'departure_planned_str': self._strptime(option.find('GeplandeVertrekTijd').text).strftime('%H:%M'),
+                'departure_actual': self._strptime(option.find('ActueleVertrekTijd').text),
+                'departure_actual_str': self._strptime(option.find('ActueleVertrekTijd').text).strftime('%H:%M'),
                 'delay': option.find('VertrekVertraging').text,
                 'train_type': option.find('ReisDeel').find('VervoerType').text
             })
         return delays
 
-    def _parse_time(self, datetime_string):
+    def _strptime(self, datetime_string):
         # parse to datetime, strip timezone
         return datetime.strptime(datetime_string[:-5], self.DATE_TIME_FORMAT).time()
+
+    def _strftime(self, date_time):
+        return datetime.strftime(date_time, self.DATE_TIME_FORMAT)
---
+++ scheduler/tasks.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from celery import Celery
+from flask import json
+from lpapp import db, app
+
+# Run with celery -A scheduler.tasks.celery worker --beat --loglevel=info
+from nsapi.api import NSApi
+
+celery = Celery('tasks')
+celery.config_from_object('scheduler.celeryconfig')
+
+@celery.task
+def check_time_frames():
+    with app.app_context():
+        for subscription_id, config in db().hgetall('train_delays:subscriptions').iteritems():
+            user_settings = json.loads(config)
+            now = datetime.now().time()
+            time_slot_begin = datetime.strptime(user_settings['time_slot_begin'], '%H:%M').time()
+            time_slot_end = datetime.strptime(user_settings['time_slot_end'], '%H:%M').time()
+            # add to api queue
+            if not db().hexists('train_delays:api_queue', subscription_id):
+                if time_slot_begin <= now <= time_slot_end:
+                    db().hset('train_delays:api_queue', subscription_id, config)
+            else:
+                db().hdel('train_delays:api_queue', subscription_id)
+
+@celery.task
+def poll_api():
+    with app.app_context():
+        api = NSApi(app.config['NS_AUTH_STRING'])
+        for subscription_id, config in db().hgetall('train_delays:api_queue').iteritems():
+            user_settings = json.loads(config)
+            today = datetime.today()
+            time_slot_begin = datetime.combine(today, datetime.strptime(user_settings['time_slot_begin'], '%H:%M').time())
+            delays = api.get(user_settings['from_station'], user_settings['to_station'], time_slot_begin)
+            print delays
+            time_slot_end = datetime.combine(today, datetime.strptime(user_settings['time_slot_end'], '%H:%M').time())
+            # filter delays to match time frame
+            delays = [d for d in delays if d['departure_actual'] <= time_slot_end]
+            #if delays:
+            print delays
+
+
+
+#def make_celery(app):
+#    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+#    celery.conf.update(app.config)
+#    TaskBase = celery.Task
+#    class ContextTask(TaskBase):
+#        abstract = True
+#        def __call__(self, *args, **kwargs):
+#            with app.app_context():
+#                return TaskBase.__call__(self, *args, **kwargs)
+#    celery.Task = ContextTask
+#    return celery
+
+#celery = make_celery(app)
